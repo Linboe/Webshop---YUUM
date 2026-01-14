ade6f0c16f1d10bc1e2a7183485d969fc31a7f53
linboe <linn.boek@gmail.com>
2026-01-14 14:28:02 +0100

    js productlist and loop

--- src/main.js
+++ src/main.js
@@ -17,43 +17,120 @@ import './style.css';
  * Datum
  */
 
-/** 10 PRODUKTER, max 3 KATEGORIER
- * Produktnamn
- * Pris
- * Rating
- * Kategori
- * Adress till bild/namn på produktbild
- */
-
 /**
- * justera antal produkter, plus minus  - EVENT
- * visa priset - totalsumman
- * visa rating under produkt
+ * PRODUKTINFO
+ * x  10 produkter (produktnamn, pris, rating, kategori, adress till bild)
+ * x  sortera produkter med tillhörande info - anv. ARRAY, OBJECT
+ * x  visa produkt och info i webläsaren - anv. LOOP
  *
+ * KATEGORIER
+ *    skapa 3 kategorier
+ *    filtrerna produkter på kategorier (se inspelning 14/1)
+ *
+ * JUSTERA ANTAL PRODUKTER
+ *    justera antal produkter, plus minus  - anv. EVENT
+ *    visa priset - totalsumman vid justering
+ *
+ * RATING
+ *    visa rating under produkt
  */
 
-/*OBJECT - samlad info för varje produkt */
-//let choc = ;
-//let glaze = ;
+// ------------------ ARRAY-OBJECT --------------------
+// ---------- samlad info för varje produkt  ----------
 
-let donuts = [
+const products = [
   {
-    name: 'Karamellchoklad',
-    images: [],
-    price: 18,
-    rating: 1,
-    category: 'sweet',
+    name: 'Chocolate',
+    images: 'img.jpg',
+    price: 20,
+    rating: 4,
+    category: 'donut',
   },
   {
-    name: 'Jordgubbsdröm',
-    images: [],
+    name: 'Vanilla',
+    images: 'img.jpg',
+    price: 20,
+    rating: 4,
+    category: 'donut',
+  },
+  {
+    name: 'Blueberry',
+    images: 'img.jpg',
     price: 28,
-    rating: 1,
-    category: 'sweet',
+    rating: 5,
+    category: 'donut',
+  },
+  {
+    name: 'Honey Swirl',
+    images: 'img.jpg',
+    price: 38,
+    rating: 5,
+    category: 'icecream',
+  },
+  {
+    name: 'Breezy melon',
+    images: 'img.jpg',
+    price: 38,
+    rating: 3,
+    category: 'icecream',
+  },
+  {
+    name: 'Salty liquorice',
+    images: 'img.jpg',
+    price: 46,
+    rating: 4,
+    category: 'icecream',
+  },
+  {
+    name: 'Marsmallow',
+    images: 'img.jpg',
+    price: 50,
+    rating: 2,
+    category: 'milkshake',
+  },
+  {
+    name: 'Strawberry',
+    images: 'img.jpg',
+    price: 50,
+    rating: 4,
+    category: 'milkshake',
+  },
+  {
+    name: 'Cocos',
+    images: 'img.jpg',
+    price: 58,
+    rating: 5,
+    category: 'milkshake',
+  },
+  {
+    name: 'Mango',
+    images: 'img.jpg',
+    price: 50,
+    rating: 5,
+    category: 'milkshake',
   },
 ];
 
-/* EVENT - för att justera antalet produkter */
+// -------------------- LOOP -----------------------
+// ----------- få ut info om produkter -------------
+
+const productListing = document.querySelector('#products');
+
+for (let i = 0; i < products.length; i++) {
+  console.log(products[i]);
+
+  const currentProduct = products[i];
+
+  productListing.innerHTML += currentProduct.name + '<br>';
+  productListing.innerHTML += currentProduct.price + '<br>';
+  productListing.innerHTML += currentProduct.rating + '<br>';
+  productListing.innerHTML += currentProduct.images + '<br>';
+  productListing.innerHTML += currentProduct.category + '<br>';
+}
+
+// -------------------- EVENT ------------------------
+// ----------- justera antalet produkter -------------
+
 const minus = document.querySelector('#subtract');
 const plus = document.querySelector('#addera');
 const currentCount = document.querySelector('#currentCount');
