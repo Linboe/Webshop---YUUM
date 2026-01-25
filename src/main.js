5812cce246ac0ad29e6c9cbb9fbdc86f269a9da0
linboe <linn.boek@gmail.com>
2026-01-25 11:48:08 +0100

    co-pilot js fix

--- src/main.js
+++ src/main.js
@@ -162,14 +162,14 @@ sortByRatingBtn.addEventListener('click', sortByRating);
 
 function sortByRating() {
   filteredProducts.sort(
-    (product1, product2) => product1.rating < product2.rating,
+    (product1, product2) => product2.rating - product1.rating,
   );
   printProducts();
 }
 
 function sortByPrice() {
   filteredProducts.sort(
-    (product1, product2) => product1.price > product2.price,
+    (product1, product2) => product1.price - product2.price,
   );
 
   printProducts();
